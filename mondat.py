020137b57ddb2158b9952bc897b75ded5f4dd2c8
Kolozsi Zsombor <kolozsizsombor1@gmail.com>
2022-09-16 20:56:47 +0200

    Week 2 homework

--- mondat.py
+++ mondat.py
@@ -3,30 +3,23 @@ def main():
     letL = []
     revd = ""
     nums = dict()
-    wrds = ""
-    wrdt = []
     n = 0
     sen = input("Adjon meg egy mondatot: ")
     for s in sen:
         letS.add(s)
         letL.append(s)
-        wrds += s
-        if s == " ":
-            wrdt.append(wrds)
-            wrds = ""
+    splitsen = sen.split()
+    print(f"Listába rendezve szavanként: {splitsen}")
     for r in reversed(sen):
         revd += r
+    print(f"Fordítva: {revd}")
     for l in letS:
-        if letL == letS:
-            n += 1
+        for i in letL:
+            if l == i:
+                n += 1
         nums[l] = n
         n = 0
-    
-    
-    return revd, nums, wrdt
-    
-
-
+    print(f"Betűk gyakorisága: {nums}")
 
 if __name__ == "__main__":
-    print(main())
+    main()
