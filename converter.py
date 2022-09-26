03f380e3d20f017c57f93983981655573a8e1b9c
Kolozsi Zsombor <kolozsizsombor1@gmail.com>
2022-09-26 08:22:27 +0200

    456

--- converter.py
+++ converter.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 def main():
     print("Adjn meg egy számot és egy mértékegységet (cm/inch): ")
     l = int(input())
@@ -15,22 +14,4 @@ def main():
     return msf
 
 if __name__ == "__main__":
-=======
-def main():
-    print("Adjn meg egy számot és egy mértékegységet (cm/inch): ")
-    l = int(input())
-    ms = input()
-    msf = 0
-    if ms == "cm":
-        msf = l * 2.54
-        return(f"{msf} inches")
-    elif ms == "inch":
-        msf = l / 2.54
-        return(f"{msf} cm")
-    else:
-        print("Not correct unit!")
-    return msf
-
-if __name__ == "__main__":
->>>>>>> ace4562bc3311e7552b10854d56de5dfea327fc9
     print(main())
