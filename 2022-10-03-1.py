8fd38842e4da83947c7d7f6da614afd267673232
Kolozsi Zsombor <kolozsizsombor1@gmail.com>
2022-10-03 08:31:16 +0200

    kjgfgkut

---
+++ 2022-10-03-1.py
@@ -0,0 +1,9 @@
+class Szuperhos:
+    def __init__(self, nev, szuperero = 50):
+        self.nev = nev
+        self.szuperero = szuperero
+        print("--Szuperhős létrehozva--")
+
+hos1 = Szuperhos("Thor",70)
+
+print()
