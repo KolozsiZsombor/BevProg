8e3556687300e711f4157086a7adc07141a60fd5
Kolozsi Zsombor <kolozsizsombor1@gmail.com>
2022-10-16 17:17:00 +0200

    b

--- developers.py
+++ developers.py
@@ -1,45 +1,55 @@
-class Developer:
-    def __init__(self, name, project, role):
-        self._name = name
-        self._project = project
-        self._role = role
-        print("--Developer létrehozva--")
-        print(f"{name} a {project}-en dolgozik {role} szerepkörben.")
+def main():
+    class Developer:
+        def __init__(self, name, project, role):
+            self._name = name
+            self._project = project
+            self._role = role
+            print("--Developer létrehozva--")
+            print(f"{name} a {project}-en dolgozik {role} szerepkörben.")
 
-developer1 = Developer("Ricsi","SolArch","Frontend")
-developer2 = Developer("Angéla","ZerTeng","Tesztelő")
-developer3 = Developer("Peti","KefERP","Backend")
-developer4 = Developer("Éva","KefERP","Frontend")
+    developer1 = Developer("Ricsi","SolArch","Frontend")
+    developer2 = Developer("Angéla","ZerTeng","Tesztelő")
+    developer3 = Developer("Peti","KefERP","Backend")
+    developer4 = Developer("Éva","KefERP","Frontend")
 
-developers = [developer1,developer2,developer3,developer4]
+    developers = [developer1,developer2,developer3,developer4]
 
-n = -1
-projects = []
-prjs = set()
-cproject = ""
-cname = ""
-kp = []
-j = None
-ip = None
+    n = -1
+    projects = []
+    prjs = set()
+    cproject = ""
+    cname = ""
+    j = None
 
-for i in developers:
-    n += 1
-    cproject = i._project
-    cname = i._name
-    projects.append([cproject,cname,n])
-    prjs.add(cproject)
+    for i in developers:
+        n += 1
+        cproject = i._project
+        cname = i._name
+        projects.append([cproject,cname,n])
+        prjs.add(cproject)
 
-k = None
+    k = None
+    Flag = False
+    same = []
+    n = 0
 
-if len(prjs) == len(projects):
-    print("Mindenki más-más projecten dolgozik.")
-else:
-	for i in projects:
-		for j in i:
-			print(j)
-		
-		# ip = 
-		# k = j
-		# j = i[0]
-		# if k == j:
-			# print(i)
+    if len(prjs) == len(projects):
+        print("Mindenki más-más projecten dolgozik.")
+        Flag = True
+
+    projects = sorted(projects)
+
+    projectDict = dict()
+
+    if not Flag:
+        for i in projects:
+            for j in prjs:
+                if i[0] == j:
+                    n += 1
+            projectDict[i] = n
+            n = 0
+                    
+    print(projectDict)
+
+if __name__ == "__main__":
+    main()
