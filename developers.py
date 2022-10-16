c90fe50672f46c64d13ee717cde4483c23038438
Kolozsi Zsombor <kolozsizsombor1@gmail.com>
2022-10-16 17:49:58 +0200

    lololololololol

--- developers.py
+++ developers.py
@@ -7,12 +7,12 @@ def main():
             print("--Developer létrehozva--")
             print(f"{name} a {project}-en dolgozik {role} szerepkörben.")
 
-    developer1 = Developer("Ricsi","SolArch","Frontend")
-    developer2 = Developer("Angéla","ZerTeng","Tesztelő")
-    developer3 = Developer("Peti","KefERP","Backend")
-    developer4 = Developer("Éva","KefERP","Frontend")
+    developer1 = Developer("Ricsi", "SolArch", "Frontend")
+    developer2 = Developer("Angéla", "ZerTeng", "Tesztelő")
+    developer3 = Developer("Peti", "KefERP", "Backend")
+    developer4 = Developer("Éva", "KefERP", "Frontend")
 
-    developers = [developer1,developer2,developer3,developer4]
+    developers = [developer1, developer2, developer3, developer4]
 
     n = -1
     projects = []
@@ -25,12 +25,11 @@ def main():
         n += 1
         cproject = i._project
         cname = i._name
-        projects.append([cproject,cname,n])
+        projects.append([cproject, cname, n])
         prjs.add(cproject)
 
     k = None
     Flag = False
-    same = []
     n = 0
 
     if len(prjs) == len(projects):
@@ -38,18 +37,36 @@ def main():
         Flag = True
 
     projects = sorted(projects)
-
     projectDict = dict()
 
     if not Flag:
         for i in projects:
-            for j in prjs:
-                if i[0] == j:
+            for j in projects:
+                if i[0] == j[0]:
                     n += 1
-            projectDict[i] = n
+            projectDict[i[0]] = n
             n = 0
-                    
-    print(projectDict)
+
+        # taking a list of values
+        v = list(projectDict.values())
+
+        # taking a list of keys
+        k = list(projectDict.keys())
+
+        m = k[v.index(max(v))]
+        same = []
+
+        for i in developers:
+            if i._project == m:
+                same.append(i._name)
+
+        n = -1
+
+        for i in same:
+            print(f"{i} " "", end="")
+
+        print(" dolgoznak egy projecten.")
+
 
 if __name__ == "__main__":
     main()
