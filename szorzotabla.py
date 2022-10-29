00e508fafe6eb98b520ce51341f6aaa6edccd6c2
Kolozsi Zsombor <kolozsizsombor1@gmail.com>
2022-10-29 21:12:09 +0200

    LOL

--- szorzotabla.py
+++ szorzotabla.py
@@ -1,11 +1,20 @@
 def main():
-    k = 1
+    print(" "*7,end="")
+    for j in range(1,13):
+        l=str(j)
+        l=l+"\t"
+        print(l.expandtabs(4),end="")
+    print()
+    print("-"*((4*12)+6))
     for i in range(1,13):
-        print()
+        l=str(i)
+        l=l+"\t"
+        print(l.expandtabs(3),":| ",end="")
         for k in range(1,13):
             txt = str(i*k)
             txt = txt+"\t"
             print(txt.expandtabs(4),end="")
-        
+        print()
+            
 if __name__ == "__main__":
     main()
