20cb9b61c05e7b01324db3328acdff8be2c826a0
Kolozsi Zsombor <kolozsizsombor1@gmail.com>
2022-10-29 18:31:22 +0200

    hw52

--- szorzotabla.py
+++ szorzotabla.py
@@ -3,7 +3,9 @@ def main():
     for i in range(1,13):
         print()
         for k in range(1,13):
-            print(i*k," ",end="")
+            txt = str(i*k)
+            txt = txt+"\t"
+            print(txt.expandtabs(4),end="")
         
 if __name__ == "__main__":
     main()
